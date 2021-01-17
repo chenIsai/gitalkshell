8044cfca59a75135c1b9698a958e93981ceca258
Isaiah Chen <58130815+chenIsai@users.noreply.github.com>
2021-01-17 04:04:45 -0500

    placeholder

--- index.js
+++ index.js
@@ -1,11 +1,86 @@
 const express = require('express')
-const app = express()
-const port = 4000
+const {exec} = require("child_process");
+const port = 4000;
+
+const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+});
+
+app.get('/test', (req, res) => {
+  exec("git log", (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+  });
+  res.sendStatus(20);
+});
+
+app.get('/git_init', (req, res) => {
+  link = "https://github.com/chenIsai/testrepo.git";
+  exec("git init", (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+    }
+    console.log("Done initializing");
+    exec(`git remote add origin ${link}`, (error, stdout, stderr) => {
+      if (error) {
+        console.log(`error: ${error.message}`);
+        return
+      }
+      if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+      }
+      console.log("Set origin");
+    });
+  });
+  res.sendStatus(200);
+});
+
+app.get('/git_commit', (req, res) => {
+  exec(`git commit -m ${message}`, (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      return
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+    }
+    console.log("Sent commit");
+  });
+  res.sendStatus(200);
 })
 
+app.get('/git_push', (req, res) => {
+  exec(`git push -u origin master`, (error, stout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      return
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+    }
+    console.log("Pushed to master");
+  });
+  res.sendStatus(200);
+})
+
+
 app.listen(port, () => {
-  console.log(`Test app listening at htpp://localhost:${port}`)
+  console.log(`Gitalk app listening at htpp://localhost:${port}`)
 })
